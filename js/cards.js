2492fd56ae607dc9a7fd1a8d4344582386b6af9b
AlexQvQ <aleksandrnelson.1900@gmail.com>
2024-02-23 20:14:55 +0300

    add task

---
+++ js/cards.js
@@ -0,0 +1,145 @@
+let orders = {
+	product: [
+		{
+			id: 1,
+			title: 'Вращающийся стул Токио',
+			price: 24000,
+			img: "img/product-photo-1@1x.jpg"
+		},
+
+		{
+			id: 2,
+			title: 'Мягкое кресло Манн-Уитни',
+			price: 12000,
+			img: "img/product-photo-2@1x.jpg"
+		},
+		
+		{
+			id: 3,
+			title: 'Кожаный диван Колумб',
+			price: 125000,
+			img: "img/product-photo-3@1x.jpg"
+		},
+
+		
+		{
+			id: 4,
+			title: 'Диван Рэдмен',
+			price: 82000,
+			img: "img/product-photo-4@1x.jpg"
+		},
+		
+		{
+			id: 5,
+			title: 'Прикроватный столик Пьер',
+			price: 16000,
+			img: "img/product-photo-5@1x.jpg"
+		},
+		
+		{
+			id: 6,
+			title: 'Стул Детройт',
+			price: 12000,
+			img: "img/product-photo-6@1x.jpg"
+		},
+		
+		{
+			id: 7,
+			title: 'Тканевый диван Альфред',
+			price:92000,
+			img: "img/product-photo-7@1x.jpg"
+		},
+
+		{
+			id: 8,
+			title: 'Обеденный комплект для кухни Модерн',
+			price: 30000,
+			img: "img/product-photo-8@1x.jpg"
+		},
+
+		{
+			id: 9,
+			title: 'Мягкое кресло Фред',
+			price: 61000,
+			img: "img/product-photo-9@1x.jpg"
+		},
+
+		{
+			id: 10,
+			title: 'Медный светильник Конг',
+			price: 10000,
+			img: "img/product-photo-10@1x.jpg"
+		},
+
+		{
+			id: 11,
+			title: 'Деревянный табурет Роган',
+			price: 2400,
+			img: "img/product-photo-11@1x.jpg"
+		},
+
+		{
+			id: 12,
+			title: 'Обеденный комплект для кухни Вууд',
+			price: 115000,
+			img: "img/product-photo-12@1x.jpg"
+		},
+		
+		{
+			id: 13,
+			title: 'Мягкое кресло Девил',
+			price: 54000,
+			img: "img/product-photo-13@1x.jpg"
+		},
+		
+		{
+			id: 14,
+			title: 'Журнальный столик Ален',
+			price: 18000,
+			img: "img/product-photo-14@1x.jpg"
+		},
+		
+		{
+			id: 15,
+			title: 'Обеденный комплект для кухни Денвер',
+			price: 86000,
+			img: "img/product-photo-15@1x.jpg"
+		},
+		
+		{
+			id: 16,
+			title: 'Мягкое кресло Барби',
+			price: 32000,
+			img: "img/product-photo-16@1x.jpg"
+		},
+		
+		{
+			id: 17,
+			title: 'Комплект для веранды Вест',
+			price: 94000,
+			img: "img/product-photo-17@1x.jpg"
+		},
+		
+		{
+			id: 18,
+			title: 'Кресло Олдмен',
+			price: 62000,
+			img: "img/product-photo-18@1x.jpg"
+		},
+		
+		{
+			id: 19,
+			title: 'Детское кресло Манго',
+			price: 8400,
+			img: "img/product-photo-19@1x.jpg"
+		},
+		
+		{
+			id: 20,
+			title: 'Деревянный комод Винди',
+			price: 36000,
+			img: "img/product-photo-20@1x.jpg"
+		},
+
+	]	
+}
